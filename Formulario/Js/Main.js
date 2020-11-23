6755e853240258d61bf03d978fb487401ec7eb52
Alexs18 <50090511+Alexs18@users.noreply.github.com>
2020-11-22 20:36:52 -0500

    Add files via upload

---
+++ Formulario/Js/Main.js
@@ -0,0 +1,11 @@
+$('.Login').click(function(){
+  $('.Formulario').animate({
+    height: "Login",
+    'padding-top': 'Login',
+    'padding-top': 'Login',
+    opacity:'Login'
+
+     
+  })
+
+});
